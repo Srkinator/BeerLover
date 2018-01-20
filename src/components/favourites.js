4b43084218a75209281c4210f799725b48319cc6
Srkinator <srki2207@gmail.com>
2018-01-20 16:35:50 +0100

    favorites feature implemented

--- src/components/favourites.js
+++ src/components/favourites.js
@@ -1,15 +1,75 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
 
 class Favourite extends Component {
     constructor(props) {
         super(props);
-        
+
+        this.state = {
+            favbeers: []
+        }
+
     }
-    
+
+
+
+    setupSearch = () => {
+        let beers = JSON.parse(localStorage.getItem("beers"));
+        let search = ""
+        console.log(beers.length);
+        for (let i = 0; i < beers.length; i++) {
+            search += beers[i]
+        }
+        fetch(`https://api.punkapi.com/v2/beers?ids=${search}`)
+            .then(result => result.json())
+            .then(result => {
+                console.log(result);
+                this.setState({
+                    favbeers: result
+                });
+            });
+    }
+
+    componentDidMount() {
+        this.setupSearch();
+    }
+
+    renderBeers = () => {
+        return (
+            this.state.favbeers.map((beer) => {
+                return (
+                    <div className="row" key={beer.id}>
+                        <div className="col-lg-4 col-md-6 col-sm-12">
+                            <h5>{beer.name}</h5>
+                            <img alt="beer" src={beer.image_url} />
+                            <p>{beer.abv}</p>
+                            <p>{beer.description}</p>
+                        </div>
+                    </div>
+                );
+            })
+        )
+    }
+
     render() {
         return (
-            <div className="favouriteContainer">
-                    Favourite beer feature is in progress...
+            <div className="container">
+                <div className="row beersMain">
+                    {this.state.favbeers.map((beer) => {
+                        return (
+                            <div className="col-xl-2 col-lg-3 col-md-3 col-sm-4 col-xs-12 beer-card" key={beer.id}>
+                                <h3>{beer.name}</h3>
+                                <img alt="beer" src={beer.image_url} />
+                                <p>{beer.abv}% alc</p>
+                                <p>{beer.description.slice(0, 150)}...</p>
+                                <Link to={`/beers/${beer.id}`}>
+                                    <button type="button" className="btn btn-info">Read More</button>
+                                </Link>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         );
     }
--- src/components/mainFeed.js
+++ src/components/mainFeed.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 
 import Search from './search';
 
-
 class MainFeed extends Component {
     constructor(props) {
         super(props);
@@ -57,12 +56,31 @@ class MainFeed extends Component {
         this.loadBeers(searchedBeer);
     }
 
-    // favouriteClassToggler =() =>{
-    //     let btn = document.getElementsByClassName("tester");
-    //     btn.setAttribute("className", "btn-primary");
-    // }
+    favouriteClassToggler = (e) => {
+        let btn = e.target;
+        let favoriteBeerID = `${e.target.getAttribute("data")}|`;
+        if (!localStorage.getItem("beers")) {
+            localStorage.setItem("beers", JSON.stringify([]));
+        }
+        let parseArray = JSON.parse(localStorage.getItem("beers"));
+        
+        if (JSON.stringify(btn.classList) == `{"0":"tester","1":"btn","2":"btn-warning"}`) {
+            btn.removeAttribute("class");
+            btn.setAttribute("class", "tester btn");
+            let index = parseArray.indexOf(favoriteBeerID);
+            if (index > -1) {
+                parseArray.splice(index, 1);
+            }
+            localStorage.setItem('beers', JSON.stringify(parseArray));
+        }
+        else {
+            btn.removeAttribute("class");
+            btn.setAttribute("class", "tester btn btn-warning");
 
-
+            parseArray.push(favoriteBeerID);
+            localStorage.setItem('beers', JSON.stringify(parseArray));
+        }
+    }
 
     render() {
         if (this.state.search.length > 0) {
@@ -72,15 +90,15 @@ class MainFeed extends Component {
                     <div className="row beersMain">
                         {this.state.search.map((beer) => {
                             return (
-                                <div className="col-lg-4 col-md-6 col-sm-6 col-xs-12" key={beer.id}>
+                                <div className="col-xl-2 col-lg-3 col-md-3 col-sm-4 col-xs-12 beer-card" key={beer.id}>
                                     <h3>{beer.name}</h3>
                                     <img alt="beer" src={beer.image_url} />
+                                    <button onClick={this.favouriteClassToggler} data={beer.id} type="button" className="btn tester">Favourite</button>
                                     <p>{beer.abv}% alc</p>
-                                    <p>{beer.description.slice(0, 150)}...</p>
+                                    <p>{beer.description.slice(0, 120)}...</p>
                                     <Link to={`/beers/${beer.id}`}>
                                         <button type="button" className="btn btn-info">Read More</button>
                                     </Link>
-                                    {/* <button className="btn btn-secondary" type="button">Favourite</button> */}
                                 </div>
                             );
                         })}
@@ -94,15 +112,15 @@ class MainFeed extends Component {
                 <div className="row beersMain">
                     {this.state.beers.slice(3, -1).map((beer) => {
                         return (
-                            <div className="col-lg-4 col-md-6 col-sm-6 col-xs-12" key={beer.id}>
+                            <div className="col-xl-2 col-lg-3 col-md-3 col-sm-4 col-xs-12 beer-card" key={beer.id}>
                                 <h3>{beer.name}</h3>
                                 <img alt="beer" src={beer.image_url} />
+                                <button onClick={this.favouriteClassToggler} data={beer.id} type="button" className="btn tester">Favourite</button>
                                 <p>{beer.abv}% alc</p>
-                                <p>{beer.description.slice(0, 150)}...</p>
+                                <p>{beer.description.slice(0, 120)}...</p>
                                 <Link to={`/beers/${beer.id}`}>
                                     <button type="button" className="btn btn-info">Read More</button>
                                 </Link>
-                                    {/* <button onClick={this.favouriteClassToggler} type="button" className="btn tester">Favourite</button> */}
                             </div>
                         );
                     })}
